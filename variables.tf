1b0bd7f3ec933c6196636012ca685a5076d05ed0
Ali9t <a.a.hadi93@gmail.com>
2021-10-07 21:58:12 -0400

    terraformmodules

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "accesskey" {
+	default="" 
+}
+
+variable "secretkey" {
+	default=""
+}
+
+variable "vpcID" {
+	default="vpc-684af403"
+}
+
+
+
+
